79b6020b27b3e37b9af563cf5f4144715789014a
malikhussain26 <hussain.am2697@gmail.com>
2024-10-14 00:44:03 -0500

    game.js

--- Game/game.js
+++ Game/game.js
@@ -2,3 +2,16 @@ export function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+export function determineWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper") ) {
+            return "You win!";
+} else {
+    return "You lose!";
+}
+}
