95436941b81ed86bcb047e25893e91a06f2d83a1
malikhussain26 <hussain.am2697@gmail.com>
2024-10-14 00:20:31 -0500

    more comments 2

--- Classes/AdventurerFactory.js
+++ Classes/AdventurerFactory.js
@@ -1,18 +1,24 @@
+
 import Adventurer from "./Adventurer.js";
 
+
 export default class AdventurerFactory {  
+  // creating role
   constructor (role) {
     this.role = role;
     this.adventurers = [];
   }
+  // creating new adventurer name
   generate (name) {
     const newAdventurer = new Adventurer(name, this.role);
     this.adventurers.push(newAdventurer);
     return newAdventurer
   }
+  // finding adventurers by index
   findByIndex (index) {
     return this.adventurers[index];
   }
+  // finding adventurers by name
   findByName (name) {
     return this.adventurers.find((a) => a.name === name);
   }
